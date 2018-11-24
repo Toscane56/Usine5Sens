80c086a63b9ce636650f21c0d5b4cde31a251595
Océane <oceanemariel@hotmail.fr>
2018-11-25 00:40:04 +0100

    Mise en place de l'accueil

--- Usine5Sens/src/pages/home/home.ts
+++ Usine5Sens/src/pages/home/home.ts
@@ -7,6 +7,15 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+	workshops = [
+	{id:0, name: "Création de parfum", description: "Créez votre parfum maison", place:"Pavillon des sens", startTime: "14:00",
+   endTime: "16:00", startDate: "25/11/2018", endDate: "30/02/2019", img:"../assets/imgs/water.jpg"},
+	{id:1, name: "Photographie et Nature", description: "Voyez la nature autrement", place:"Place principal - jet d'eau", startTime: "19:00",
+   endTime: "17:00", startDate: "25/11/2018", endDate: "30/02/2019", img:"../assets/imgs/water.jpg"},
+	{id:2, name: "Atelier Cuisine", description: "Testez de nouvelles recettes", place:"Halle d'Auteuil", startTime: "14:00",
+   endTime: "22:00", startDate: "25/11/2018", endDate: "30/02/2019", img:"../assets/imgs/water.jpg"},
+	]
+
   constructor(public navCtrl: NavController) {
 
   }
