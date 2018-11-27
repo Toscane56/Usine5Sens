0c940aa31577c96d96cfd418147c63de741e2e64
Océane <oceanemariel@hotmail.fr>
2018-11-27 20:35:10 +0100

    Gestion des comptes en cours

--- Usine5Sens/src/pages/gestion-compte/gestion-compte.ts
+++ Usine5Sens/src/pages/gestion-compte/gestion-compte.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import{VisitePage} from "../visite/visite";
+import{AteliersPage} from "../ateliers/ateliers";
+import{HomePage} from "../home/home";
+import{SignupPage} from "../signup/signup";
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
+
 
 /**
  * Generated class for the GestionComptePage page.
@@ -15,11 +21,46 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class GestionComptePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	token = this.authServiceProvider.token;
+	coData = {"email":"", "password":""}; //tableau permettant de récupérer les données de connexion de l'utilisateur
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad GestionComptePage');
-  }
+	constructor(public navCtrl: NavController,   public authServiceProvider : AuthServiceProvider) {
+	}
+
+	ionViewDidLoad() {
+	    console.log("token :"+this.authServiceProvider.token);
+	    //console.log(this.authServiceProvider.email);
+	}
+
+	  activerEcranVisite(){
+	    //Fonction permettant d'amener à la page visite de l'Usine des 5 Sens
+	    this.navCtrl.push(VisitePage);
+	  }
+
+	  activerEcranAteliers(){
+	    //Fonction permettant d'amener à la page regroupant tous les ateliers
+	    this.navCtrl.push(AteliersPage);
+	  }
+
+	  activerAccueil(){
+	    //Fonction permettant d'amener à l'accueil
+	    this.navCtrl.push(HomePage);
+	  }
+
+	  connexion(){
+	  this.authServiceProvider.token =1;
+	  this.navCtrl.setRoot(GestionComptePage);
+	  }
+
+	  activerSignup(){
+	    //Fonction permettant d'amener à l'écran d'inscription
+	    this.navCtrl.push(SignupPage);
+	  }
+
+	  deconnexion(){
+	  //Fonction permettant de déconnecter l'utilisateur et l'amène de nouveau sur la page d'accueil
+	  	this.authServiceProvider.token = undefined; //arret de la session
+	  	this.navCtrl.push(HomePage); //active l'écran de la page d'accueil
+	  }
 
 }
--- Usine5Sens/src/pages/signup/signup.ts
+++ Usine5Sens/src/pages/signup/signup.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import{VisitePage} from "../visite/visite";
+import{AteliersPage} from "../ateliers/ateliers";
+import{HomePage} from "../home/home";
+import{GestionComptePage} from "../gestion-compte/gestion-compte";
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
 /**
  * Generated class for the SignupPage page.
  *
@@ -15,11 +19,40 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+	token = this.authServiceProvider.token;
+	registerData = {"surname":"", "name":"", "email":"", "password":""}; //tableau permettant de récupérer les données de connexion de l'utilisateur
+
+	constructor(public navCtrl: NavController,   public authServiceProvider : AuthServiceProvider) {
+	}
+
+	ionViewDidLoad() {
+	    console.log("token :"+this.authServiceProvider.token);
+	    //console.log(this.authServiceProvider.email);
+	}
+
+  activerEcranVisite(){
+    //Fonction permettant d'amener à la page visite de l'Usine des 5 Sens
+    this.navCtrl.push(VisitePage);
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SignupPage');
+  activerEcranAteliers(){
+    //Fonction permettant d'amener à la page regroupant tous les ateliers
+    this.navCtrl.push(AteliersPage);
+  }
+
+  activerAccueil(){
+    //Fonction permettant d'amener à l'accueil
+    this.navCtrl.push(HomePage);
+  }
+
+  activerGestionCompte(){
+    //Fonction permettant d'amener à la page servant à gérer le compte
+    this.navCtrl.push(GestionComptePage);
+  }
+
+  register(){
+  this.authServiceProvider.token =1;
+  this.navCtrl.push(GestionComptePage);
   }
 
 }
