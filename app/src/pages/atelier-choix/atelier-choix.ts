97d61ba9e842dff04cbe6f994ce403256b9f1774
Océane <oceanemariel@hotmail.fr>
2019-01-19 14:25:47 +0100

    Reservation à un atelier ok

--- app/src/pages/atelier-choix/atelier-choix.ts
+++ app/src/pages/atelier-choix/atelier-choix.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 import{AteliersPage} from "../ateliers/ateliers";
 import{GestionComptePage} from "../gestion-compte/gestion-compte";
 import{HomePage} from "../home/home";
+import{SigninPage} from "../signin/signin";
 import{VisitePage} from "../visite/visite";
 import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
 import { RequestServiceProvider } from '../../providers/request-service/request-service';
@@ -24,14 +26,17 @@ export class AtelierChoixPage {
 
   token = this.authServiceProvider.token;
 	workshopArray:any;
+  workshopId = {"workshop_id":""};
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public requestServiceProvider : RequestServiceProvider, public authServiceProvider : AuthServiceProvider) {
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public navParams: NavParams,public requestServiceProvider : RequestServiceProvider, public authServiceProvider : AuthServiceProvider) {
     this.workshopArray = navParams.get('Workshop'); //récupération du tableau workshop de la page précédente 
+    this.workshopId.workshop_id = this.workshopArray.id ;
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AtelierChoixPage');
-    console.log(this.workshopArray);  
+    console.log(this.workshopArray);
+    console.log(this.token);
      
   }
 
@@ -68,5 +73,60 @@ export class AtelierChoixPage {
     //Fonction permettant de retourner à la page précédente
     this.navCtrl.pop();
   }  
+
+  reserverAtelier(){
+    //Fonction permettant la réservation d'un atelier
+    if (this.token == null) //si l'utilisateur n'est pas connecté
+    {
+      this.navCtrl.push(SigninPage);
+    }else{
+        this.envoiDonneesReservation();
+    }
+  }
+
+  envoiDonneesReservation(){
+    //Fonction permettant de créer un lien entre l'utilisateur et l'atelier réservé dans la base de données
+      console.log(this.workshopArray.id);
+
+      //Envoi au serveur le json   
+          this.authServiceProvider.request('workshop', 'book' , this.workshopId).then((result) => {
+          console.log("J'ai envoyé les donnees.")
+
+              this.presentToastReservation();
+              //sinon passer à l'écran suivant
+
+          }, (error) => {
+          //erreur coté serveur
+            console.log(error);
+            console.log("ça ne marche pas. L'id du workshop est : "+this.workshopId);
+          });
+    }  
+
+  presentToastReservation() {
+    //Définit le message de refus de se connecter sans toutes les données
+    let toast = this.toastCtrl.create({
+        message: "Inscription effectuée !",
+        duration: 3000
+      });
+    toast.present();
+  }
+
+  // changementInteret(event){
+  //   //Permet de changer l'intéret de l'utilisateur pour un évenement de la ville 
+  //   let bouton = event.target;
+  //   console.log(event.target);
+  //   let identifiant = $(event.target.closest('.card')).attr('id');//Récupere l'id de l'evenement
+  //   if($(bouton).attr('value') =='Pas intéressé(e)'){ //Si la valeur de l'attribut value était "pas intéressé(e)"
+  //     $(bouton).attr('value', 'Intéressé(e)'); //Alors changer en "interessé(e)"
+  //     //bouton.innerHTML = 'Intéressé(e)';
+
+  //   }else{ //sinon
+  //     console.log("changement : pas interet");
+  //     $(bouton).attr('value', 'Pas intéressé(e)'); //Alors changer en "pas interessé(e)"
+  //     //bouton.innerHTML = 'Pas intéressé(e)';  
+  //   }
+  //   let card = $(event.target.closest('.card'));
+  //   card.removeClass("active"); //Fermer l'évenement
+  // }
 
 }
--- app/src/pages/signin/signin.ts
+++ app/src/pages/signin/signin.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { ToastController } from 'ionic-angular';
+import{VisitePage} from "../visite/visite";
+import{AteliersPage} from "../ateliers/ateliers";
+import{HomePage} from "../home/home";
+import{GestionComptePage} from "../gestion-compte/gestion-compte";
+import{SignupPage} from "../signup/signup";
+import { RequestServiceProvider } from '../../providers/request-service/request-service';
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
 /**
  * Generated class for the SigninPage page.
  *
@@ -15,11 +22,98 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SigninPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	token = this.authServiceProvider.token;
+	coData = {"email":"", "password":""}; //tableau permettant de récupérer les données de connexion de l'utilisateur
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SigninPage');
-  }
+	constructor(public navCtrl: NavController, public requestServiceProvider : RequestServiceProvider, public authServiceProvider : AuthServiceProvider, public toastCtrl: ToastController) {
+	}
+
+	ionViewDidLoad() {
+	    console.log("token :"+this.authServiceProvider.token);
+	    //console.log(this.authServiceProvider.email);
+	}
+
+	activerEcranVisite(){
+    	//Fonction permettant d'amener à la page visite de l'Usine des 5 Sens
+    	this.navCtrl.push(VisitePage);
+	}
+
+	activerEcranAteliers(){
+    	//Fonction permettant d'amener à la page regroupant tous les ateliers
+    	this.navCtrl.push(AteliersPage);
+	}
+
+	activerAccueil(){
+    	//Fonction permettant d'amener à l'accueil
+    	this.navCtrl.push(HomePage);
+	}
+
+	activerGestionCompte(){
+		//Fonction permettant d'amener à la page servant à gérer le compte
+		this.navCtrl.push(GestionComptePage);
+	}
+
+	connexion(){
+    //Fonction permettant d'enregistrer un nouveau utilisateur dans la base de données
+    	console.log(this.coData);
+	    if(this.coData.email == "" || this.coData.password == "" ){
+	    //Si au moins 1 des champs est vide
+	    this.presentToastFormulaireIncomplet();
+	    //Transmettre un message à l'utilisateur
+	    }else{
+	    //Envoi au serveur le json   
+	      	this.requestServiceProvider.request('user', 'login' ,this.coData).then((result) => {
+	      	console.log("J'ai envoyé les donnees.")
+	          	//Récupere le token de l'utilisateur
+	          	var $my_token = JSON.parse(result['_body']).user.token;
+	          	this.authServiceProvider.token = $my_token;
+
+	          	//Récupere son prenom
+	          	var firstname = JSON.parse(result['_body']).user.firstname;          
+	          	this.authServiceProvider.firstname = firstname;
+
+	            this.navCtrl.setRoot(HomePage);
+	          	//sinon passer à l'écran suivant
+
+	      	}, (error) => {
+	        //erreur coté serveur
+	          console.log(error);
+	          console.log("ça ne marche pas");
+	          this.presentToastConnexion();
+	      	});
+	    }
+  	}
+
+	presentToastFormulaireIncomplet() {
+    //Définit le message de refus de se connecter sans toutes les données
+    let toast = this.toastCtrl.create({
+        message: "Veuillez remplir tout le formulaire",
+        duration: 3000
+      });
+    toast.present();
+	}
+
+	presentToastConnexion() {
+    //Définit le message "Pseudo ou mot de passe incorrecte" lorsqu'un utilisateur essaye de se connecter
+    //avec des erreurs
+    let toast = this.toastCtrl.create({
+        message: "Pseudo ou mot de passe incorrecte",
+        duration: 3000
+      });
+    toast.present();
+	}
+
+
+	activerSignup(){
+	    //Fonction permettant d'amener à l'écran d'inscription
+	    this.navCtrl.push(SignupPage);
+	}
+
+	deconnexion(){
+	  //Fonction permettant de déconnecter l'utilisateur et l'amène de nouveau sur la page d'accueil
+	  	this.authServiceProvider.token = undefined; //arret de la session
+	  	this.navCtrl.push(HomePage); //active l'écran de la page d'accueil
+	}
+
 
 }
